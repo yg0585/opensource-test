6c87554d8fb19c96b99d24952136e81a5f71ded9
yg0585 <yg0585@naver.com>
2023-02-09 22:27:41 +0900

    a

--- src/main/java/lib/TestLibrary.java
+++ src/main/java/lib/TestLibrary.java
@@ -2,7 +2,7 @@ package lib;
 
 public class TestLibrary {
     public static String helloWorld(boolean flag){
-        int tmp = 21;
+        int tmp = 22;
         if (flag) {
             return "hello world!";
         }
