5e0514e54e64d409713956dc6e0567a872112b6d
yg0585 <yg0585@naver.com>
2023-02-09 17:22:33 +0900

    pr deco test

--- src/main/java/lib/TestLibrary.java
+++ src/main/java/lib/TestLibrary.java
@@ -2,6 +2,7 @@ package lib;
 
 public class TestLibrary {
     public static String helloWorld(boolean flag){
+        int tmp = 0;
         if (flag) {
             return "hello world!";
         }
